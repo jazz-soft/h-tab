f80cf515f54c210ec403ab8fd740d1dc3302f410
Sema <jazz-dev@jazz-soft.net>
2024-06-07 18:05:02 -0400

    Fixed warnings

--- h-tab.mjs
+++ h-tab.mjs
@@ -1,7 +1,6 @@
 import {EditorView, basicSetup} from "codemirror"
-import {javascript} from "@codemirror/lang-javascript"
 
 let editor = new EditorView({
-  extensions: [basicSetup, javascript()],
+  extensions: [basicSetup],
   parent: document.body
 })
